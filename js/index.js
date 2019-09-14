72747cb4a1db9c01d12cf9ce3fcc624b95ed5b1f
johannesvollmer <johannes.boenki@t-online.de>
2019-09-14 15:39:04 +0200

    Fix color conversion and prototyp curve svg

--- js/index.js
+++ js/index.js
@@ -4,6 +4,7 @@ function main(){
     const canvas = document.getElementById("gl")
     const loading = document.getElementById("loading")
     const original = document.getElementById("original")
+    const path = document.getElementById("curve-path")
 
     const gl = canvas.getContext("webgl2")
     const background = 0.1
@@ -31,14 +32,13 @@ function main(){
             0.0193339 * srgb.r + 0.1191920 * srgb.g + 0.9503041 * srgb.b
         );
 
-        return xyz / vec3(94.811, 100.0, 107.304);
+        return xyz / vec3(94.811, 100.0, 107.304); // reference white
     }
 
+    // http://www.brucelindbloom.com/index.html?Eqn_XYZ_to_RGB.html
     vec3 xyz_to_rgb(vec3 xyz) {
-        xyz *= vec3(94.811, 100.0, 107.304);
-
-        // TODO companding?  see http://www.brucelindbloom.com/index.html?Eqn_XYZ_to_RGB.html
-        return srgb_to_rgb(vec3(  // TODO make this a matrix multiplication?
+        xyz *= vec3(94.811, 100.0, 107.304); // reference white
+        return srgb_to_rgb(vec3( // TODO companding? // TODO make this a matrix multiplication?
             3.24045420 * xyz.x - 1.5371385 * xyz.y - 0.4985314 * xyz.z,
             -0.9692660 * xyz.x + 1.8760108 * xyz.y + 0.0415560 * xyz.z,
             0.05564340 * xyz.x - 0.2040259 * xyz.y + 1.0572252 * xyz.z
@@ -46,59 +46,54 @@ function main(){
     }
 
     vec3 xyz_to_lab(vec3 xyz) {
+        float thrd = 1.0 / 3.0;
         float e = 216.0 / 24389.0;
         float k = 24389.0 / 27.0;
-        float thrd = 1.0 / 3.0;
 
         float fx = xyz.x > e ? pow(xyz.x, thrd) : (k*xyz.x + 16.0) / 116.0;
         float fy = xyz.y > e ? pow(xyz.y, thrd) : (k*xyz.y + 16.0) / 116.0;
         float fz = xyz.z > e ? pow(xyz.z, thrd) : (k*xyz.z + 16.0) / 116.0;
 
-        return vec3(
-            166.0 * fy - 16.0,
-            500.0 * (fx - fy),
-            200.0 * (fy - fz)
-        );
+        return vec3(116.0 * fy - 16.0, 500.0 * (fx - fy), 200.0 * (fy - fz));
     }
 
+
+    // http://www.brucelindbloom.com/index.html?Eqn_Lab_to_XYZ.html
     vec3 lab_to_xyz(vec3 lab) {
         float e = 216.0 / 24389.0;
         float k = 24389.0 / 27.0;
-        float l = lab.x;
-        float a = lab.y;
-        float b = lab.z;
 
-        // http://www.brucelindbloom.com/index.html?Eqn_Lab_to_XYZ.html
-        float fy = (l + 16.0) / 166.0;
-        float fz = fy - (b / 200.0);
-        float fx = a / 500.0 + fy;
-
-        float fxp3 = pow(fx, 3.0);
-        float fzp3 = pow(fz, 3.0);
+        float fy = (lab.x + 16.0) / 116.0;
+        float fz = fy - (lab.z / 200.0);
+        float fx = lab.y / 500.0 + fy;
+        float fx3 = pow(fx, 3.0);
+        float fz3 = pow(fz, 3.0);
 
         return vec3(
-            fxp3 > e ? fxp3 : (116.0 * fx - 16.0) / k,
-            l > k*e ? pow((l + 16.0) / 116.0, 3.0) : l / k,
-            fzp3 > e ? fzp3 : (116.0 * fz - 16.0) / k
+            fx3 > e ? fx3 : ((116.0 * fx - 16.0) / k),
+            lab.x > k*e ? pow((lab.x + 16.0) / 116.0, 3.0) : lab.x / k,
+            fz3 > e ? fz3 : (116.0 * fz - 16.0) / k
         );
     }
 
     vec3 rgb_to_lab(vec3 rgb) {
-        // return xyz_to_lab(rgb_to_xyz(rgb));
-        return rgb_to_xyz(rgb);
+        return xyz_to_lab(rgb_to_xyz(rgb));
     }
 
     vec3 lab_to_rgb(vec3 lab) {
-        // return xyz_to_rgb(lab_to_xyz(lab));
-        return xyz_to_rgb(lab);
+        return xyz_to_rgb(lab_to_xyz(lab));
     }
     `
 
+
+
+    
     const fragment = compileShader(gl.FRAGMENT_SHADER, `#version 300 es
     precision highp float;
     uniform sampler2D source; // TODO store as LAB colors
     
     uniform sampler2D offsetByLuminance; // unset these if not modifying?
+    uniform float offsetByLuminanceFactor;
 
     in vec2 pixel;
     out vec4 fragColor;
@@ -116,8 +111,8 @@ function main(){
             vec4 sourcePixel = texture(source, pixel);
             vec3 lab = rgb_to_lab(sourcePixel.rgb);
 
-            // float luminance = lab.y;
-            // lab += texture(offsetByLuminance, vec2(luminance, 0.5)).xyz;
+            float luminance01 = lab.x / 130.0; // TODO divide by max XYZ Y value
+            lab += (texture(offsetByLuminance, vec2(luminance01, 0.5)).xyz - 0.5) * 100.0 * offsetByLuminanceFactor;
 
             fragColor = vec4(lab_to_rgb(lab), sourcePixel.a); // TODO alpha curves?
         }
@@ -145,6 +140,11 @@ function main(){
     const viewScaleUniform = gl.getUniformLocation(program, "viewScale")
     const viewOffsetUniform = gl.getUniformLocation(program, "viewOffset")
 
+    const sourceTextureUniform = gl.getUniformLocation(program, "source")
+
+    const luminanceOffsetMapUniform = gl.getUniformLocation(program, "offsetByLuminance")
+    const luminanceOffsetFactorUniform = gl.getUniformLocation(program, "offsetByLuminanceFactor")
+
     const vertexPositionAttribute = gl.getAttribLocation(program, "vertex")
     gl.enableVertexAttribArray(vertexPositionAttribute)
 
@@ -156,10 +156,54 @@ function main(){
     gl.vertexAttribPointer(vertexPositionAttribute, 2, gl.FLOAT, false, 0, 0)
 
 
+    const curve = Array(256).fill(128)
+
+    const points = [ {x:0, y:0.3, size: 0.001}, {x:0.5, y:0.5, size: 0.003}, {x:1, y:-0.3, size: 0.005} ]
+    updateSVGFromPoints()
+
+
+    const curveMap = gl.createTexture()
+    gl.bindTexture(gl.TEXTURE_2D, curveMap)
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR)
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR)
+    updateCurveData()
+
+    // path.setAttributeNS(null, "d", "M 1,97.857143 C 19.285714,96.428571 24.016862,131.64801 90.714286,132.85714 140.78762,133.7649 202.79376,66.16041 202.79376,66.16041")
+
+    function updateSVGFromPoints(){
+        for(let index = 0; index < curve.length; index++){
+            const x = index / (curve.length - 1)
+
+            let value = 0
+
+            for(let point of points){
+                const distanceToPoint = Math.abs(x - point.x)
+                const wheight = Math.pow(2, (-distanceToPoint*distanceToPoint) / point.size) // simple gaussian
+                value += wheight * point.y
+            }
+
+            curve[index] = value * 0.5 + 0.5
+        }
+
+        const pathString = "M 0," + (1-curve[0]) + " " + curve.map((y, x) => "L " + (x / (curve.length - 1)) + "," + (1-y)).join(" ") 
+        path.setAttributeNS(null, "d", pathString)
+    }
+
+    function updateCurveData(){
+        const height = 1
+        const width = curve.length
+        gl.bindTexture(gl.TEXTURE_2D, curveMap)
+        gl.texImage2D(
+            gl.TEXTURE_2D, 0, gl.RGBA, width, height, 
+            0, gl.RGBA, gl.UNSIGNED_BYTE, 
+            new Uint8Array(curve.flatMap(y => [y * 256, 128, 128, 128]))
+        )    
+    }
+
+
     // when uploaded, contains the open gl texture id
     let image = null
     let imageAspect = 1
-    let canvasAspect = 1
     
     input.addEventListener("change", event => {
         updateSourceImageFromFile(event.target.files)
@@ -237,7 +281,6 @@ function main(){
     listen(window, "resize", () => {
         canvas.width = window.innerWidth
         canvas.height = window.innerHeight
-        canvasAspect = canvas.width / canvas.height
         gl.viewport(0, 0, canvas.width, canvas.height)
         draw()
     })
@@ -252,17 +295,26 @@ function main(){
             gl.vertexAttribPointer(vertexPositionAttribute, 2, gl.FLOAT, false, 0, 0)
             gl.useProgram(program)
     
-            const overallScale = 1.5
+            const overallScale = 1.1
+            const canvasAspect = canvas.width / canvas.height
             const aspect = imageAspect / canvasAspect
-            const offsetX = 0.3 // (1 / aspect - 1) * overallScale //  / imageAspect // TOOD +1?
+            const offsetX = 0// .3 // (1 / aspect - 1) * overallScale //  / imageAspect // TOOD +1?
 
             // scale the image according to aspect ratio and fit it into the view
             const scale = aspect > 1? [overallScale, -overallScale * aspect] : [overallScale / aspect, -overallScale]
             gl.uniform2fv(viewScaleUniform, scale)
 
             gl.uniform2f(viewOffsetUniform, -offsetX, 0)
+
+            gl.uniform1i(sourceTextureUniform, 0)
+            gl.activeTexture(gl.TEXTURE0)
             gl.bindTexture(gl.TEXTURE_2D, image)
-            
+
+            gl.uniform1f(luminanceOffsetFactorUniform, 1)
+            gl.uniform1i(luminanceOffsetMapUniform, 1)
+            gl.activeTexture(gl.TEXTURE1)
+            gl.bindTexture(gl.TEXTURE_2D, curveMap)
+
             gl.drawArrays(gl.TRIANGLES, 0, vertexData.length / 2)
         }
     }
